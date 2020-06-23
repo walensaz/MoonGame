6f17dcd996258db04aeea2bed736510e466fe527
Zachary <zwalensa@gmail.com>
2020-06-23 14:40:54 -0500

    Added SessionConnectEventName, more universal

--- gameserver/scalaserver/session/SessionConnectEvent.scala
+++ gameserver/scalaserver/session/SessionConnectEvent.scala
@@ -5,14 +5,18 @@ import java.net.Socket
 import scalaserver.event.{EventExecutor, Event}
 
 case class SessionConnectEvent(socket: Socket) extends Event {
-  override def eventName: String = "session.connect.event"
+  override def eventName: String = SessionConnectEventName.name
 }
 
 class SessionConnectEventExecutor extends EventExecutor {
-  val eventName: String = "session.connect.event"
+  val eventName: String = SessionConnectEventName.name
 
   def execute(sessionConnectEvent: Event): Unit = {
     val event = sessionConnectEvent.asInstanceOf[SessionConnectEvent]
     val newSession: Session = Session.createNewSession(event.socket)
   }
+}
+
+object SessionConnectEventName {
+  val name = "session.connect.event"
 }
